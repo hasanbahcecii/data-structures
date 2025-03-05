36202c6e4774d58f192637cba3fdc6a3de9ad9c3
Hasan BAHÇECİ <hasan.bahceci021@gmail.com>
2025-03-05 13:54:35 +0300

    create binarySearch

---
+++ binarySearch.java
@@ -0,0 +1,48 @@
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] myList = {87, 21, 45, 93};
+
+        System.out.println(binSearch(myList, 93)); // Should output index
+        System.out.println(binSearch(myList, 16)); // Should output -1
+    }
+
+    public static int binSearch(int[] list, int item) {
+        if (isListEmpty(list)) {
+            return -1;
+        }
+
+        int low = 0;
+        int high = list.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int guess = list[mid];
+
+            if (guessEqualsItem(guess, item)) {
+                return mid;
+            } else if (guessGreaterThanItem(guess, item)) {
+                high = mid - 1;
+            } else if (guessLessThanItem(guess, item)) {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static boolean isListEmpty(int[] myList) {
+        return myList.length == 0;
+    }
+
+    public static boolean guessEqualsItem(int guess, int item) {
+        return guess == item;
+    }
+
+    public static boolean guessGreaterThanItem(int guess, int item) {
+        return guess >= item;
+    }
+
+    public static boolean guessLessThanItem(int guess, int item) {
+        return guess <= item;
+    }
+}
